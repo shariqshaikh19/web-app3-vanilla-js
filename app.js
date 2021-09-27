30852d90d9de1b88caa6838c3f86cf894be34675
shariqshaikh19 <shariqshaikh77@gmail.com>
2021-09-27 22:37:18 +0530

    style: styles added to cash management

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-var billAmount= document.querySelector("#bill-amount");
-var cashGiven= document.querySelector("#cash-given");
-var message= document.querySelector("#errorMessage");
-var validateButton= document.querySelector("#validate");
-var numberOfNotes= document.querySelector(".no-of-notes");
+const billAmount= document.querySelector("#bill-amount");
+const cashGiven= document.querySelector("#cash-given");
+const message= document.querySelector("#errorMessage");
+const validateButton= document.querySelector("#validate");
+const numberOfNotes= document.querySelectorAll(".no-of-notes");
 
-var notesAvailable= [2000,500,100,50,10,1];
+const notesAvailable= [2000,500,100,50,20,10,1];
 
 validateButton.addEventListener("click", function clickHandler(){
     hideMessage();
-    if(billAmount.value >= 0){
-        if(cashGiven.value >= billAmount.value){
-            var changeToBeGiven= cashGiven.value - billAmount.value;
+    if(billAmount.value > 0)
+    {
+        if(cashGiven.value >= billAmount.value)
+        {
+            console.log("hello");
+            const changeToBeGiven= cashGiven.value - billAmount.value;
             calculateChange(changeToBeGiven);
         }
-        else{
-            showMessage("Cash given should be more than the bill amount")
+        else
+        {
+            showMessage("Cash given should be more than the bill amount");
         }
     }
-    else{
+    else
+    {
         showMessage("Please enter a valid amount");
     }
-})
+});
 
 function calculateChange(amount){
-    for(var i=0; i<notesAvailable.length; i++){
-        var calculateNotes= Math.trunc(amount / notesAvailable[i]);
+    for(let i=0; i<notesAvailable.length; i++){
+        const calculateNotes= Math.trunc(amount / notesAvailable[i]);
         amount= amount % notesAvailable[i];
         numberOfNotes[i].innerText=calculateNotes;
     }
